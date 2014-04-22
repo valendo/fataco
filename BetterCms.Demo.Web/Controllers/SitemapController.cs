791a62ab4222ed63bfcebb31e9823f159c2ab202
phuongnt <trangphuong618@gmail.com>
2014-04-22 23:05:36 +0700

    temp

--- BetterCms.Demo.Web/Controllers/SitemapController.cs
+++ BetterCms.Demo.Web/Controllers/SitemapController.cs
@@ -91,7 +91,7 @@ namespace BetterCms.Demo.Web.Controllers
                             if (item.ChildrenNodes.Count > 0)
                             {
                                 sb.Append("<div class=\"col\">");
-                                sb.Append("<h2>"+item.Title+"</h2>");
+                                sb.Append("<h2>" + item.Title + "</h2>");
                                 sb.Append("<ul>");
                                 foreach (var item1 in item.ChildrenNodes)
                                 {
@@ -113,19 +113,19 @@ namespace BetterCms.Demo.Web.Controllers
         }
 
 
-     
+
         public ActionResult ChangeCulture(string lang, string returnUrl)
         {
             Session["Culture"] = new CultureInfo(lang);
             using (var api = ApiFactory.Create())
             {
-                var request =new  GetPageTranslationsRequest {PageUrl=returnUrl };
-                 
-                var xx=api.Pages.Page.Translations.Get(request);
+                var request = new GetPageTranslationsRequest { PageUrl = returnUrl };
+
+                var xx = api.Pages.Page.Translations.Get(request);
                 if (xx.Data.TotalCount > 1)
                 {
 
-                    List<PageTranslationModel> pages1 =  xx.Data.Items.ToList();
+                    List<PageTranslationModel> pages1 = xx.Data.Items.ToList();
                     foreach (var item in pages1)
                     {
                         string language = item.LanguageCode != null ? item.LanguageCode : "vi";
@@ -141,48 +141,92 @@ namespace BetterCms.Demo.Web.Controllers
 
         public virtual ActionResult SubMenu(string parentUrl)
         {
+            //var menuItems = new List<MenuItemViewModel>();
+
+            //using (var api = ApiFactory.Create())
+            //{
+            //    var pageRequest = new PageExistsRequest { PageUrl = parentUrl };
+            //    var pageResponse = api.Pages.Page.Exists(pageRequest);
+
+            //    var sitemapId = GetSitemapId(api);
+            //    if (sitemapId.HasValue)
+            //    {
+            //        var parentRequest = new GetSitemapNodesRequest();
+            //        parentRequest.SitemapId = sitemapId.Value;
+            //        parentRequest.Data.Take = 1;
+            //        parentRequest.Data.Filter.Add("ParentId", null);
+
+            //        var filter = new DataFilter(FilterConnector.Or);
+            //        parentRequest.Data.Filter.Inner.Add(filter);
+            //        filter.Add("Url", parentUrl);
+            //        if (pageResponse.Data.Exists)
+            //        {
+            //            filter.Add("PageId", pageResponse.Data.PageId.Value);
+            //        }
+            //        parentRequest.Data.Order.Add("DisplayOrder");
+
+            //        var parentResponse = api.Pages.Sitemap.Nodes.Get(parentRequest);
+            //        if (parentResponse.Data.Items.Count == 1)
+            //        {
+            //            var request = new GetSitemapTreeRequest { SitemapId = sitemapId.Value };
+            //            request.Data.NodeId = parentResponse.Data.Items[0].Id;
+            //            var response = api.Pages.Sitemap.Tree.Get(request);
+            //            if (response.Data.Count > 0)
+            //            {
+            //                menuItems = response.Data.Select(mi => new MenuItemViewModel { Caption = mi.Title, Url = mi.Url }).ToList();
+            //                //menuItems.Insert(0, new MenuItemViewModel { Caption = "Main", Url = parentUrl });
+            //            }
+            //        }
+            //    }
+            //}
+
+            //return View(menuItems);
             var menuItems = new List<MenuItemViewModel>();
 
             using (var api = ApiFactory.Create())
             {
-                var pageRequest = new PageExistsRequest { PageUrl = parentUrl };
-                var pageResponse = api.Pages.Page.Exists(pageRequest);
-
                 var sitemapId = GetSitemapId(api);
                 if (sitemapId.HasValue)
                 {
-                    var parentRequest = new GetSitemapNodesRequest();
-                    parentRequest.SitemapId = sitemapId.Value;
-                    parentRequest.Data.Take = 1;
-                    parentRequest.Data.Filter.Add("ParentId", null);
+                    var request = new GetSitemapTreeRequest { SitemapId = sitemapId.Value, Data = new GetSitemapTreeModel { LanguageId = LanguageHelper.CurrentLanguageId } };
 
-                    var filter = new DataFilter(FilterConnector.Or);
-                    parentRequest.Data.Filter.Inner.Add(filter);
-                    filter.Add("Url", parentUrl);
-                    if (pageResponse.Data.Exists)
+                    var response = api.Pages.Sitemap.Tree.Get(request);
+                    System.Text.StringBuilder sb = new System.Text.StringBuilder();
+                    if (response.Data.Count > 0)
                     {
-                        filter.Add("PageId", pageResponse.Data.PageId.Value);
-                    }
-                    parentRequest.Data.Order.Add("DisplayOrder");
-
-                    var parentResponse = api.Pages.Sitemap.Nodes.Get(parentRequest);
-                    if (parentResponse.Data.Items.Count == 1)
-                    {
-                        var request = new GetSitemapTreeRequest { SitemapId = sitemapId.Value };
-                        request.Data.NodeId = parentResponse.Data.Items[0].Id;
-                        var response = api.Pages.Sitemap.Tree.Get(request);
-                        if (response.Data.Count > 0)
+                        foreach (var item in response.Data)
                         {
-                            menuItems = response.Data.Select(mi => new MenuItemViewModel { Caption = mi.Title, Url = mi.Url }).ToList();
-                            //menuItems.Insert(0, new MenuItemViewModel { Caption = "Main", Url = parentUrl });
+                            if (item.ChildrenNodes.Count > 0)
+                            {
+                                if (item.Url == parentUrl)
+                                {
+                                    sb.Append("<div id=\"VMENU709487940\" class=\"VMENU\">");
+                                    sb.Append("    <ul class=\"VMENU_0 VMENU_0_709487940 reset\">");
+                                    foreach (var item1 in item.ChildrenNodes)
+                                    {
+                                        sb.Append("       <li>");
+                                        sb.Append("       <a href=\""+item1.Url+"\" class=\"" + (System.Web.HttpContext.Current.Request.Path == item1.Url ? "\"a_s\"" : null) + "\"><span class=\"" + (System.Web.HttpContext.Current.Request.Path == item1.Url ? "\"s\"" : null) + "\">" + item1.Title + "</span></a>");
+                                        sb.Append("       </li>");
+                                        sb.Append("         <li class=\"d\"></li>");
+                                    }
+                                    sb.Append("    </ul>");
+                                    sb.Append("</div>");
+
+
+                                }
+
+                            }
                         }
+                        menuItems = response.Data.Select(mi => new MenuItemViewModel { Caption = mi.Title, Url = mi.Url }).ToList();
                     }
+                    ViewBag.MenuString2 = sb.ToString();
                 }
             }
 
-            return View(menuItems);
+            return View();
         }
 
+       
         private Guid? GetSitemapId(IApiFacade api)
         {
             var allSitemaps = api.Pages.Sitemap.Get(new GetSitemapsRequest());
